fbb5bf86526aa388c05382df830944be872328ac
Cristian Rodrigo Estrada Romero <ecristian963@gmail.com>
2022-05-26 00:47:06 -0500

    chat model
    chat model add and user match model update

--- conectados/lib/models/user_match_model.dart
+++ conectados/lib/models/user_match_model.dart
@@ -1,16 +1,40 @@
 // ignore_for_file: prefer_const_constructors_in_immutables
 
+import 'package:conectados/models/models.dart';
 import 'package:equatable/equatable.dart';
 
 class UserMatch extends Equatable {
   final int id;
   final int userId;
   final User matchedUser;
-  //final List<Chat>? chat;
+  final List<Chat>? chat;
 
-  UserMatch(
-      {required this.id, required this.userId, required this.matchedUser});
+  UserMatch({
+    required this.id,
+    required this.userId,
+    required this.matchedUser,
+    required this.chat,
+  });
 
   @override
   List<Object?> get props => [id, userId, matchedUser];
+
+  static List<UserMatch> matches = [
+    UserMatch(
+      id: 1,
+      userId: 1,
+      matchedUser: User.users[1],
+      chat: Chat.chats
+          .where((chat) => chat.userId == 1 && chat.otherUserId == 2)
+          .toList(),
+    ),
+    UserMatch(
+      id: 1,
+      userId: 1,
+      matchedUser: User.users[1],
+      chat: Chat.chats
+          .where((chat) => chat.userId == 1 && chat.otherUserId == 2)
+          .toList(),
+    )
+  ];
 }
